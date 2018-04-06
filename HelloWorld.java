67c6e79500b0f649be4c070121f107b8c465c2ed
Ryan Lauzon <lauzon232@gmail.com>
2018-04-05 22:52:34 -0400

    hello world update 2

--- HelloWorld.java
+++ HelloWorld.java
@@ -2,5 +2,6 @@ java class hello world
 
 public static void main (String [] args)
 {
-  system.out.println("Hello world, how's it going?")
+  system.out.println("Hello world, how's it going?");
+  system.out.printon("Hey there");
 }
